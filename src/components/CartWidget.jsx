faf8bef7ccc71afc40da6727572ac0d6f78150e9
AMQ-uiroga <amquiroga2025@gmail.com>
2025-09-13 23:39:11 -0300

    Primera Entrega de React cambios para deployar

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,7 +1,7 @@
 export const CartWidget = () => (
   <>
     <img
-      src={"src/assets/logoStore.png"}
+      src={"repo-React/src/assets/logoStore.png"}
       alt="logo de la tienda de color negro y rosa con la imagen de una remera y una bolsa que dice Clothing Store."
     />
     <div className="carrito-icono">
