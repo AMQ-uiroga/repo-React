79b0a7bdc136fbbc044279f2d26d82e99a821974
AMQ-uiroga <amquiroga2025@gmail.com>
2025-09-14 00:05:15 -0300

    Se cambia ubicacion de Logo y se borrar las imagenes no usadas

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import "./App.css";
 import { CartWidget } from "./components/CartWidget";
 import { ItemListContainer } from "./components/ItemListContainer";
 import { NavBar } from "./components/NavBar";
+import logo from "./assets/logoStore.png";
 
 function App() {
   return (
     <>
       <div className="header">
         <NavBar />
+        <img
+          src={logo}
+          alt="logo de la tienda de color negro y rosa con la imagen de una remera y una bolsa que dice Clothing Store."
+        />
         <CartWidget />
       </div>
       <ItemListContainer saludo="Bienvenido Proximamente a la Tienda Online" />
